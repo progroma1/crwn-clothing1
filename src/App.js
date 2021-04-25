46704ed08bb688301b980b3d1734dacc7efe333d
Roman <tyniv@progroma.com>
2021-04-25 20:35:03 +0300

    updating with a shop page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import React from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
+import ShopPage from './pages/shop/shop.component';
 
 
   
-const HatsPage = () => {
-  return (
-    <div>
-      <h1> HatsPage </h1>
-    </div>
-  )
-};
+
 
 
 function App() {
@@ -20,7 +14,7 @@ function App() {
     <div>
       <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
       </Switch>
          
     </div>
