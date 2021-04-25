1faf4b326081cd206cb905cf4226519cfb269e52
Roman <tyniv@progroma.com>
2021-04-25 20:57:30 +0300

    update with first version of header component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
   
@@ -12,6 +13,7 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
