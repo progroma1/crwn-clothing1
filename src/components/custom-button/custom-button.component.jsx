cc5062224d4105d1eb37866b5d90095aeabf0bd1
Roman <tyniv@progroma.com>
2021-05-10 15:20:00 +0300

    added redux implemented userReducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './custom-button.styles.scss';
 
 
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : '' } custom-button`} {...otherProps}>
+    <button
+        type="button" 
+        className={`${isGoogleSignIn ? 'google-sign-in' : '' } custom-button`} {...otherProps}>
         {children}
     </button>
 );
