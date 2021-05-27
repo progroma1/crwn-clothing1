ff6b232e85cfbab3117f0b6dbdbca377f37f337d
Roman <tyniv@progroma.com>
2021-05-27 23:24:04 +0300

    adding checkout page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -45,6 +46,7 @@ render () {
       <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={ () => this.props.currentUser ? (
               <Redirect to="/" />
               ) : (
