0f0b4481403d01771b724678ccb57c91a0354f8a
Roman <tyniv@progroma.com>
2021-05-29 15:42:07 +0300

    styling checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,10 +7,11 @@ import { withRouter } from 'react-router-dom';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component'
 import {selectCartItems} from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, ...otherProps }) => (
     <div className="cart-dropdown">
         <div className="cart-items">
             {
