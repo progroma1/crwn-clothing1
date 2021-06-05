401191725537ff9bf417b4b3b7ce8ad3bba11ea6
Roman <tyniv@progroma.com>
2021-06-05 21:37:30 +0300

    refactoring how collection is being rendered

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,14 @@
-import React, { Component } from 'react';
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import React from 'react';
 
-class ShopPage extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-    render() { 
-        const {collections} = this.state;
-        return ( <div className='shop-page'>
-                    {
-                        collections.map(({id, ...otherCollectionProps}) => (
-                            <CollectionPreview key={id}{...otherCollectionProps} />
-                        ))}
-                 </div> );
-    }
-}
  
+
+const ShopPage = ({ collections }) => 
+( <div className='shop-page'>
+    <CollectionsOverview />
+</div> )
+
+
+
 export default ShopPage;
