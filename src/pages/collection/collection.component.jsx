5bc1993c27d96d048a76f051d5adf83e11c0fa01
Roman <tyniv@progroma.com>
2021-06-19 22:37:03 +0300

    data normalization

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -11,11 +11,16 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 
 const CollectionPage = ({collection}) => {
 
-    console.log(collection);
+    const { title, items } = collection;
 
     return (
         <div className='collection-page'>
-            <h2> PAGE</h2>
+            <h2 className='title'> {title} </h2>
+            <div className="items">
+                {
+                    items.map( item => (<CollectionItem key={item.id} item={item} />) )
+                }
+            </div>
         </div>
     )
 }
