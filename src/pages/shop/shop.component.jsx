1896f3467d7177a782d9be261e8552a4d963e6a8
Roman <tyniv@progroma.com>
2021-06-19 14:22:35 +0300

    adding category page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CategoryPage from '../category/category.component';
 
- 
 
-const ShopPage = ({ collections }) => 
-( <div className='shop-page'>
-    <CollectionsOverview />
-</div> )
+const ShopPage = ({match}) => {
+    console.log(match)
+    return ( 
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionsOverview} />
+            <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+        </div> 
+    )
+
+}
 
 
 
