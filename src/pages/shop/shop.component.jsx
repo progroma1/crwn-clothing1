977790ce5c2b694bd465d5e1ed6bd40c6d51fa0d
Roman <tyniv@progroma.com>
2021-06-19 15:13:07 +0300

    improvements for collection page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CategoryPage from '../category/category.component';
+import CollectionPage from '../collection/collection.component';
 
 
 const ShopPage = ({match}) => {
     console.log(match)
     return ( 
-        <div className='shop-page'>
+        <div className='collection-page'>
             <Route exact path={`${match.path}`} component={CollectionsOverview} />
-            <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
         </div> 
     )
 
